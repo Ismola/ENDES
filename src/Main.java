1a5173d209f0cef3721a2d9b55a9e1d1f959e4b2
Ismola <Ismolahiena@gmail.com>
2021-03-21 17:16:34 +0100

    Esta va a ser la segunda subida

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es de "+ saldoActual + " €");
 
         try {
             miCuenta.retirar(2300);
